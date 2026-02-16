b73b71eef8681d2288f00de0d658979ab948c652
albertoDabu <albertodb95@gmail.com>
2026-02-16 19:21:21 +0100

    Fix: rename useAuth.js to .jsx and other JSX files con cambio ruta

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Login from './components/Login.jsx'
-import { AuthProvider, useAuth } from './hooks/useAuth.js'
+import { AuthProvider, useAuth } from './hooks/useAuth.jsx'
 
 // Componente que maneja el routing autenticado
 function AppWithAuth() {
